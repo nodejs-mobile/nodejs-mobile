b6094ce5bcd92e7c884d4c48e42682153a39c7a7
Jaime Bernardo <jaime@janeasystems.com>
2019-12-18 17:19:08 +0100

    ios,test: use ios-deploy output commands
    New versions of ios-deploy accept arguments to specify files to write
    the application's stdout and stderr streams to.
    Use these arguments to avoid ios-deploy timing issues when running the
    tests.

--- tools/mobile-test/ios/node-ios-proxy.sh
+++ tools/mobile-test/ios/node-ios-proxy.sh
@@ -10,12 +10,19 @@ TEMP_COMMAND="$BASH_SOURCE $@"
 
 PROXY_BASE_DIR="$( cd "$( dirname "$0" )" && pwd )"
 LOG_FILE_PATH="$PROXY_BASE_DIR/testsrun_$DEVICE_ID.log"
+STDOUT_FILE_PATH="$PROXY_BASE_DIR/stdout_$DEVICE_ID.log"
+STDERR_FILE_PATH="$PROXY_BASE_DIR/stderr_$DEVICE_ID.log"
 IOS_APP_PATH="$PROXY_BASE_DIR/Release-iphoneos/testnode.app"
 
 TEST_BASE_DIR="$( cd "$( dirname "$0" )" && cd .. && cd .. && cd test && pwd )"
 
 echo "Time: $(date '+%FT%T') -> Proxying testcase: $TEMP_COMMAND" >> "$LOG_FILE_PATH"
 
-ios-deploy -t 240 --noinstall $TARGET_DEVICE -b "$IOS_APP_PATH" --noninteractive --args "--substitute-dir $TEST_BASE_DIR $*" | sed $'s/\r$//' | tee -a "$LOG_FILE_PATH" | sed '1,/(lldb)     autoexit/d' | sed -E '/Process [0-9]+ exited with status.*|PROCESS_EXITED/,$d'
+ios-deploy -t 240 --noinstall $TARGET_DEVICE -b "$IOS_APP_PATH" --output "$STDOUT_FILE_PATH" --error_output "$STDERR_FILE_PATH" --noninteractive --args "--substitute-dir $TEST_BASE_DIR $*" | sed $'s/\r$//' | tee -a "$LOG_FILE_PATH" | sed '1,/(lldb)     autoexit/d' | sed -E '/Process [0-9]+ exited with status.*|PROCESS_EXITED/,$d'
 
-exit ${PIPESTATUS[0]}
+OUTPUT_VALUE=${PIPESTATUS[0]}
+
+cat "$STDOUT_FILE_PATH" | sed $'s/\r$//' >&1
+cat "$STDERR_FILE_PATH" | sed $'s/\r$//' >&2
+
+exit $OUTPUT_VALUE
