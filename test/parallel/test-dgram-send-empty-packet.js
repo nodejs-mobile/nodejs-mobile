5d382cd649a1bce7e77563a2f6916cc0e4b613de
Jaime Bernardo <jaime@janeasystems.com>
2020-01-14 17:40:10 +0100

    test,ios: skip tests that send empty UDP packets
    macOS 10.15 Catalina and iOS 13 have a bug which causes empty UPD
    packets not to work. This seems to be a regression for a bug that
    affected macOS 10.10.
    
    Ref: https://github.com/nodejs/node/issues/30030

--- test/parallel/test-dgram-send-empty-packet.js
+++ test/parallel/test-dgram-send-empty-packet.js
@@ -1,6 +1,6 @@
 'use strict';
 const common = require('../common');
-if (common.isOSX)
+if (common.isOSX || common.isIOS)
   common.skip('because of 17894467 Apple bug');
 
 const assert = require('assert');
